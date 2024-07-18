9a20951ae64dcb6bbec9639a935930046b606304
pat717 <ap5884789@gmail.com>
2024-07-18 15:02:16 +0530

    This is python program

---
+++ third.py
@@ -0,0 +1,17 @@
+#sqaure program:
+
+#print the square of 1 to 6.
+#lis = [1,2,3,4,5,6]
+# for i in lis:
+#     print(i*i)
+#print([i*i for i in lis]) #list comprehension
+#print({i:i*i for i in lis}) #dict comprehension
+
+#square program
+
+# print the square of 1 to 6
+list = [1,2,3,4,5,6]
+for i in list:
+    print(i*i)
+ #   print([i*i for i in list]) # list comprehension
+ #   print([i:i*i for i in list]) # dict comprehension
